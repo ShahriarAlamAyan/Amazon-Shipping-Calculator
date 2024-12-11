679040cefa00d84cd1668c20bcbf498422f7f9d9
Ayan <shahriaralamayan@gmail.com>
2024-12-11 12:04:06 +0600

    input fix

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function costInput() {
     const inputElement = document.querySelector(".js-cost-input");
+    
     let cost = inputElement.value
+    
+    cost = Number(cost)    
     if (cost < 40) {
         cost = cost + 10
     }
+    
+    document.querySelector('.js-total-cost').innerHTML = '$' + cost
     console.log(cost);
     
 }
